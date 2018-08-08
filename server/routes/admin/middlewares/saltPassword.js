5a58a2c40fb024049f576e93a0cc4dee66322aa7
Ivan Koryakovtsev <beatzhitta@gmail.com>
2018-08-08 19:17:02 +0300

    admin middleware & entity end-points improvements

--- server/routes/admin/middlewares/saltPassword.js
+++ server/routes/admin/middlewares/saltPassword.js
@@ -1,19 +1,21 @@
 const password = require("password-hash-and-salt");
 const { ADMIN_PASSWORD } = require("../../../models/Admin/consts");
+const { errCode0006 } = require("../../../errors");
 
-module.exports = (req, res, next) => 
-    password(req.matchedData[ADMIN_PASSWORD] + process.env.SECRET)
-        .hash((error, hash) => {
-            if (error) {
-                /**
-                 * Unhandled error when salting
-                 */
-                return res.status(500).json(errCode0006(error));
-            }
+module.exports = (req, res, next) =>
+	password(req.matchedData[ADMIN_PASSWORD] + process.env.SECRET).hash(
+		(error, hash) => {
+			if (error) {
+				/**
+				 * Unhandled error when salting
+				 */
+				return res.status(500).json(errCode0006(error));
+			}
 
-            /**
-             * Store hashed password to db
-             */
-            req.matchedData[ADMIN_PASSWORD] = hash;
-            return next();
-        })
+			/**
+			 * Store hashed password to db
+			 */
+			req.matchedData[ADMIN_PASSWORD] = hash;
+			return next();
+		}
+	);
--- server/routes/admin/middlewares/validateRegister.js
+++ server/routes/admin/middlewares/validateRegister.js
@@ -1,15 +1,12 @@
-const {
-    ADMIN_PASSWORD,
-    ADMIN_LOGIN
-} = require("../../../models/Admin/consts");
+const { ADMIN_PASSWORD, ADMIN_LOGIN } = require("../../../models/Admin/consts");
 
 const { check } = require("express-validator/check");
 
 module.exports = [
-    check(ADMIN_PASSWORD)
-        .exists()
-        .withMessage("Заполните поле \"Пароль\""),
-    check(ADMIN_LOGIN)
-        .exists()
-        .withMessage("Заполните поле \"Логин\""),
-]
+	check(ADMIN_PASSWORD)
+		.exists()
+		.withMessage("Заполните поле \"Пароль\""),
+	check(ADMIN_LOGIN)
+		.exists()
+		.withMessage("Заполните поле \"Логин\"")
+];
