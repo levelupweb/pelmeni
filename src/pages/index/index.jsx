c24b680808f268cb3e616bb1714b9fb6c73f2715
kv9991 <beatzhitta@gmail.com>
2018-11-30 18:21:23 +0300

    fix

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -45,36 +45,6 @@ const IndexPage = () => (
 	<Page className={styles.pageWrapper} fluid={true}>
 		<div className={styles.wrapper}>
 			{/** 
-			This svg for title bordering
-			with curve
-		*/}
-
-			<svg
-				style={{ visibility: "hidden", position: "absolute" }}
-				width="0"
-				height="0"
-				xmlns="http://www.w3.org/2000/svg"
-				version="1.1"
-			>
-				<defs>
-					<filter id="goo">
-						<feGaussianBlur
-							in="SourceGraphic"
-							stdDeviation="10"
-							result="blur"
-						/>
-						<feColorMatrix
-							in="blur"
-							mode="matrix"
-							values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9"
-							result="goo"
-						/>
-						<feComposite in="SourceGraphic" in2="goo" operator="atop" />
-					</filter>
-				</defs>
-			</svg>
-
-			{/** 
 			First slide
 			Index slide with jumbotron
 		*/}
