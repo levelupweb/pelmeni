cce1363bcc4b61440794ae3da779a91017e28dc3
kv9991 <beatzhitta@gmail.com>
2019-01-15 08:47:26 +0300

    fix smtp

--- server/email/server.js
+++ server/email/server.js
@@ -1,8 +1,9 @@
 const emailjs = require("emailjs");
 
 module.exports = emailjs.server.connect({
-  user:	process.env.SMTP_USER, 
-  password: process.env.SMTP_PASSWORD, 
-  host:	process.env.SMTP_HOST, 
-  ssl: process.env.SSL,
+	user: process.env.SMTP_USER,
+	password: process.env.SMTP_PASSWORD,
+	host: process.env.SMTP_HOST,
+	port: 465,
+	ssl: true
 });
